bfca6f946f5805655217ab48a382152a090ac6c0
Graham Wheeler <>
2025-06-18 15:26:00 -0700

    Don't clobber existing output files

--- arxivsummary/arxivsummary.py
+++ arxivsummary/arxivsummary.py
@@ -26,7 +26,14 @@ def ids_file(topics: list[str]) -> str:
 
 
 def report_file(date_range, topics: list[str]) -> str:
-    return os.path.join(f"arxiv_summary_{'_'.join(sorted(topics))}_{date_range}.md")
+    count = 1
+    extra = ''
+    while True:
+        fname = os.path.join(f"arxiv_summary_{'_'.join(sorted(topics))}_{date_range}{extra}.md")
+        if not os.path.exists(fname):
+            return fname
+        extra = f"-{count}"
+        count += 1
 
 
 # Load previously analyzed paper IDs
@@ -46,7 +53,7 @@ def save_analyzed_ids(ids, topics: list[str]) -> None:
 
 # Analyze a paper using OpenAI ChatCompletion
 def analyze_paper(title, abstract, topics, client, model, verbose) -> bool:
-    assert(client is not None)
+    assert (client is not None)
     retry = 0
     results = []
     while retry < MAX_RETRIES:
@@ -100,7 +107,7 @@ def extract_text_from_pdf(pdf_file) -> str | None:
 
 # Summarize paper text using OpenAI ChatCompletion
 def summarize_text(text, client, model) -> str | None:    
-    assert(client is not None)
+    assert (client is not None)
     retry = 0
     while retry < MAX_RETRIES:
         try:
@@ -121,10 +128,11 @@ def summarize_text(text, client, model) -> str | None:
 
 # Generate Markdown summary
 def output_report(out, papers, date_range, topics, include_summaries, include_json) -> None:
+    fname = report_file(date_range, topics)
     if out == '--':
         f = sys.stdout
     else:
-        f = open(out if out else report_file(date_range, topics), "w")
+        f = open(out if out else fname, "w")
 
     f.write(f"# arXiv Paper Summary ({datetime.now().strftime('%Y-%m-%d')})\n\n")
     f.write(f"### Examined Papers Date Range: {date_range}\n\n")
@@ -138,17 +146,17 @@ def output_report(out, papers, date_range, topics, include_summaries, include_js
     for paper in papers:
         f.write(f"<a name=\"{paper['target']}\">\n")
         if include_summaries:
-            f.write(f"## [{paper['title']}](#summary-of-{paper['target']})\n</a>\n")       
-            f.write(f"**Link:** [{paper['link']}]({paper['link']})\n\n")      
+            f.write(f"## [{paper['title']}](#summary-of-{paper['target']})\n</a>\n")
+            f.write(f"**Link:** [{paper['link']}]({paper['link']})\n\n")
         else:
-            f.write(f"## [{paper['title']}]({paper['link']})\n</a>\n")              
+            f.write(f"## [{paper['title']}]({paper['link']})\n</a>\n")
         f.write(f"**Abstract:** {paper['abstract']}\n\n")
         f.write(f"**Analysis:** {paper['analysis']}\n\n")
         f.write("---\n\n")
 
     if include_summaries:
         for paper in papers:
-            f.write(f"<a name=\"summary-of-{paper['target']}\">\n")        
+            f.write(f"<a name=\"summary-of-{paper['target']}\">\n")
             f.write(f"## Summary of {paper['title']}\n</a>\n")
             f.write(f"**Link:** [{paper['link']}]({paper['link']})\n\n")
             f.write(f"**Summary:** {paper['summary']}\n\n")
@@ -158,12 +166,14 @@ def output_report(out, papers, date_range, topics, include_summaries, include_js
         f.close()
 
     if include_json and out != '--':
-        json_file = out.replace('.md', '.json') if out else report_file(date_range, topics).replace('.md', '.json')
+        json_file = out.replace('.md', '.json') if out \
+            else fname.replace('.md', '.json')
         with open(json_file, "w") as json_f:
             json.dump(papers, json_f, indent=4)
 
 
-def parse_model(model: str, local_client: openai.OpenAI, openai_client: openai.OpenAI) -> tuple[openai.OpenAI, str]:
+def parse_model(model: str, local_client: openai.OpenAI, \
+                openai_client: openai.OpenAI) -> tuple[openai.OpenAI, str]:
     client, model_name = model.split('/')
     if client.strip() == 'openai':
         return openai_client, model_name.strip()
@@ -173,18 +183,19 @@ def parse_model(model: str, local_client: openai.OpenAI, openai_client: openai.O
 
 def generate_report(topics: list[str],
                     token: str|None = os.environ.get("OPENAI_TOKEN"),
-                    out: str|None = None, 
-                    verbose:bool = False, 
+                    out: str|None = None,
+                    verbose: bool = False,
                     show_all: bool = False,
-                    max_entries: int = -1, 
+                    max_entries: int = -1,
                     persistent: bool = True,
                     classify_model: str = 'ollama/phi4',
                     summarize_model: str = '',
                     include_json: bool = False,
                     ):
-    local_client = openai.OpenAI(base_url='http://localhost:11434/v1/', api_key='ollama')
+    local_client = openai.OpenAI(base_url='http://localhost:11434/v1/',\
+                                 api_key='ollama')
     if not token:
-        token = os.environ.get('OPENAI_API_KEY')    
+        token = os.environ.get('OPENAI_API_KEY')
     if token:
         openai_client = openai.OpenAI(api_key=token)        
     classify_client, classify_model = parse_model(classify_model, local_client, openai_client)
@@ -242,11 +253,11 @@ def generate_report(topics: list[str],
                     "link": entry.link,
                     "analysis": include,
                     "summary": summary,
-                    "target": re.sub(r'[^a-z\-]', '', title.lower().replace(' ','-'))   
+                    "target": re.sub(r'[^a-z\-]', '', title.lower().replace(' ', '-'))   
                 })
             count += 1
             if max_entries >= 0 and count >= max_entries:
-                break               
+                break
 
     if persistent and not show_all:
         save_analyzed_ids(analyzed_ids, topics)
